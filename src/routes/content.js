1ec7d6c599bbd01d22829c731c3da4d2be37887f
chebread <83535893+chebread@users.noreply.github.com>
2022-07-22 21:07:57 +0900

    backup: 2

--- src/routes/content.js
+++ src/routes/content.js
@@ -1,28 +1,42 @@
 import renderHTML from '../components/renderHTML.js';
 import request from '../components/request.js';
 import isSelected from '../components/isSelected.js';
-import useState from '../components/useState.js';
 
+// path parameter type => users/years/ (or users/)
 const content = path => {
-  // path parameter type => users/years/ (or users/)
+  const [data, setData] = useState(1); // 어떻게 render 할 것인가?
+  const html = `
+    <div><a href="/">Back to the home</a></div>
+    <div id="commits"></div>
+    <div id="years"></div>
+  `;
+  renderHTML(html, document.querySelector('#root')); // static!
   const pathArray = path.split('/');
   const username = pathArray[0];
-  const parameterYear = pathArray[1];
-  const thisYear = new Date().getFullYear();
-  // static html content rendering
-  const html = `
-      <div><a href="/">Back to the home</a></div>
-      <div id="commits"></div>
-      <div id="years"></div>
-    `;
-  renderHTML(html, document.querySelector('#root'));
-  // dynamic logic
-  const render = () => {
-    let totalContributions = 0;
-    const content = `${username} is ${totalContributions} commits`;
-    renderHTML(content, document.querySelector('#commits'));
+  const parameterYear =
+    pathArray[1] === undefined ? new Date().getFullYear() : pathArray[1]; // if parameter has no data, append this year data
+  const query = {
+    query: `query {
+          user(login: "${username}") {
+            ${`contributionsCollection(from: "${parameterYear}-01-01T00:00:00", to: "${parameterYear}-12-01T00:00:00")`} {
+              contributionYears
+              contributionCalendar {
+                totalContributions
+              }
+            }
+          }
+        }`,
   };
-  render();
+  (async () => {
+    const userData = await request(query).then(data => {
+      return data;
+    }); // 이것을 setData 해야 함
+    const totalContributions =
+      userData.data.user.contributionsCollection.contributionCalendar
+        .totalContributions;
+    const commits = `${username} is ${totalContributions} commits`;
+    renderHTML(commits, document.querySelector('#commits'));
+  })();
 };
 
 export default content;
