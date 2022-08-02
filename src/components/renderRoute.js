006b75a563412e0bb2b5b3b9f84338e4cc0d385b
Cha Haneum <developer_haneum_11@icloud.com>
2022-08-02 20:54:23 +0900

    backup: 3

--- src/components/renderRoute.js
+++ src/components/renderRoute.js
@@ -4,7 +4,7 @@ const renderRoute = path => {
   // title이 존재 하지 않을 수는 없어서 그냥 이렇게 처리
   // home route가 아니면 모두 - memere 를 title에 붙입니다.
   // if (!(path.indexOf('@') === -1))
-  routes(path).route(); // 동적 라우트 URL parameter
+  routes(path).route(); // 특정 컴포넌트를 실행합니다
   // else routes(path).route();
 };
 
--- src/components/util.js
+++ src/components/util.js
@@ -1,3 +1,50 @@
-const util = () => {};
+import router from './router.js';
 
-export default util;
+const util = () => {
+  // 클로저의 문제가 많음.
+  // 동적으로 url 받아오도록 해야 함
+  // const debounceFrame = callback => {
+  //   // console.log(1);
+  //   let nextFrameCallback = -1;
+  //   return () => {
+  //     cancelAnimationFrame(nextFrameCallback);
+  //     nextFrameCallback = requestAnimationFrame(callback);
+  //   };
+  // };
+
+  const options = {
+    currentStateKey: 0,
+    states: [],
+  };
+
+  const useState = (initState, c) => {
+    const { currentStateKey: key, states } = options;
+    if (states.length === key) states.push(initState);
+
+    const state = states[key];
+    const setState = newState => {
+      states[key] = newState;
+      c();
+      options.currentStateKey = 0;
+    };
+    options.currentStateKey += 1;
+    return [state, setState];
+  };
+
+  // render가 url을 받도록 클로저를 구현해야 한다
+  // const _render = debounceFrame(() => {
+  //   const url = window.location.pathname;
+  //   router(url);
+  //   options.currentStateKey = 0;
+  // });
+
+  // const render = () => {
+  //   _render();
+  // };
+
+  return { useState };
+};
+
+const { useState } = util();
+
+export { useState };
