db39ea91b737bd2212f180475445f6d808ddb14b
Cha Haneum <developer_haneum_11@icloud.com>
2022-08-21 12:51:39 +0900

    blank

--- src/components/isYearExcess.js
+++
@@ -1,8 +0,0 @@
-const isYearExcess = year => {
-  const utcYear = new Date().getUTCFullYear();
-  const isParameter = year === undefined ? false : true;
-  if (isParameter && (year < 2008 || year > utcYear)) return true;
-  return false;
-};
-
-export default isYearExcess;
--- src/routes/request.js
+++ src/routes/request.js
@@ -13,20 +13,20 @@ const request = async () => {
     Authorization: `token ${API_KEY}`,
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
   };
+  // collection에서 to 생략하면 from에서 딱 1년 계산됨
+  // 요청해야 하는 것은 user의 날마다의 total commit임 (이것을 어떻게 불러올 것인가)
+  // 그리고 이것을 object로 하여 graph에 toss의 stock graph 처럼 확인한다
   const query = {
-    query: `query {
-      user(login: "${username}") {
-        ${`contributionsCollection(from: "${
-          isParameter ? parameterYear : utcYear
-        }-01-01T00:00:00", to: "${
-          isParameter ? parameterYear : utcYear
-        }-12-31T23:59:59")`} {
-          contributionCalendar {
-            totalContributions
+    query: `
+      query {
+        user(login: "${username}") {
+          ${`contributionsCollection(from: "${
+            isParameter ? parameterYear : utcYear
+          }-01-01T00:00:00")`} {
+            totalCommitContributions
           }
         }
-      }
-    }`,
+      }`,
   };
   const response = await fetch(url, {
     method: 'POST',
@@ -38,3 +38,27 @@ const request = async () => {
 };
 
 export default request;
+
+// totalCommitContributions
+
+// user(login: "${username}") {
+// ${`contributionsCollection(from: "${
+//   isParameter ? parameterYear : utcYear
+// }-01-01T00:00:00", to: "${
+//   isParameter ? parameterYear : utcYear
+// }-12-31T23:59:59")`} {
+//     commitContributionsByRepository {
+//       contributions {
+//         totalCount
+//       }
+//     }
+//   }
+// }
+
+// contributionCalendar {
+//   weeks {
+//     contributionDays {
+//       contributionCount
+//     }
+//   }
+// }
