98d7f89b7ffe60ca72bbe89bca0f74f77cdd6900
chebread <83535893+chebread@users.noreply.github.com>
2022-08-31 11:57:13 +0900

    4

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,6 @@ import webpack from 'webpack';
 import fs from 'fs-extra';
 import paths from '../config/paths.js';
 import webpackConfig from '../config/webpack.config.js';
-import '../config/env.js';
 
 const config = webpackConfig('production'); // start는 원래 development 환경에서 진행하기 때문에 그냥 인자를 이것으로만 줘도 괜찮다.
 const compiler = webpack(config);
